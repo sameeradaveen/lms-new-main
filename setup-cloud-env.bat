1aebe851b8274543cba4cf8ca0954cc6eeb6b14d
sameera <r190486@rguktrkv.ac.in>
2025-07-19 15:35:04 +0530

    Add MongoDB Atlas cloud configuration and environment setup

---
+++ setup-cloud-env.bat
@@ -0,0 +1,4 @@
+MONGODB_URI=mongodb+srv://r190486:U8iJk3RnAZAFYQ6Y@cluster0.s9pbtnw.mongodb.net/nextgenfreeedu?retryWrites=true&w=majority&appName=Cluster0
+NODE_ENV=production
+PORT=3000
+JWT_SECRET=your_jwt_secret_here_change_this_in_production
